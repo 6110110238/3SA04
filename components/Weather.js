585a072181680ffb2e40ea384f5fd4571cfb4cd3
Nuntipat Pantarag <6110110238@psu.ac.th>
2020-08-25 09:19:34 +0700

    Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react' ;
+import React, { useState, useEffect } from 'react' ;
 import { StyleSheet, Text, View, ImageBackground  } from 'react-native' ;
 import Forecast from './Forecast';
 
@@ -8,7 +8,25 @@ export default function Weather(props) {
     description: '-',
     temp: 0
     })
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=a1e33c56217fe047828ab5be26041f58`)
+        .then((response) => response.json())
+        .then((json) => {
+        setForecastInfo({
+        main: json.weather[0].main,
+        description: json.weather[0].description,
+        temp: json.main.temp
+        });
+        })
+        .catch((error) => {
+        console.warn(error);
+        });
+        }
+        }, [props.zipCode])
     return (
+        
         <View>
  <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
  <Text>Zip Code</Text>
